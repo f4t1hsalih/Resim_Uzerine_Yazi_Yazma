f699923b35936b92f0767d0fd82bcedd65953890
f4t1hsalih <f4t1hsalih@gmail.com>
2023-08-23 14:23:53 +0300

    Tasarım Tamamlandı

---
+++ Resim_Uzerine_Yazi_Yazma/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Resim_Uzerine_Yazi_Yazma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            colorDialog1 = new ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.ForeColor = Color.FromArgb(56, 52, 67);
+            button1.Location = new Point(12, 54);
+            button1.Name = "button1";
+            button1.Size = new Size(163, 36);
+            button1.TabIndex = 0;
+            button1.Text = "Renk Seç";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.ForeColor = Color.FromArgb(56, 52, 67);
+            button2.Location = new Point(12, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(163, 36);
+            button2.TabIndex = 1;
+            button2.Text = "Resim Seç";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.ForeColor = Color.FromArgb(56, 52, 67);
+            button3.Location = new Point(12, 96);
+            button3.Name = "button3";
+            button3.Size = new Size(163, 36);
+            button3.TabIndex = 2;
+            button3.Text = "Yazır";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.ForeColor = Color.FromArgb(56, 52, 67);
+            button4.Location = new Point(12, 138);
+            button4.Name = "button4";
+            button4.Size = new Size(163, 36);
+            button4.TabIndex = 3;
+            button4.Text = "Kaydet";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.FromArgb(180, 214, 202);
+            label1.Location = new Point(12, 187);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 21);
+            label1.TabIndex = 4;
+            label1.Text = "Metin: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.FromArgb(180, 214, 202);
+            label2.Location = new Point(12, 222);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 21);
+            label2.TabIndex = 5;
+            label2.Text = "Boyut: ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(75, 184);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 29);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(75, 219);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 29);
+            textBox2.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.Location = new Point(199, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(339, 236);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(65, 75, 89);
+            ClientSize = new Size(560, 270);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = Color.FromArgb(56, 52, 67);
+            Margin = new Padding(4);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Resmin Üzerine Yazı Yazma";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private ColorDialog colorDialog1;
+    }
+}
